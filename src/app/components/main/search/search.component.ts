71f0a26fc319201c739af586f14515b4d343be32
Simax Saab <minoreat@gmail.com>
2022-05-05 02:30:14 +0300

    feat: add rxjs to filter

--- src/app/components/main/search/search.component.ts
+++ src/app/components/main/search/search.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { search } from 'src/app/reducers/search';
 
+import { fromEvent, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -10,13 +13,39 @@ import { search } from 'src/app/reducers/search';
 export class SearchComponent implements OnInit {
 
   public searchStr!: string;
+  public toogleSearch: boolean = true;
+  public toggleText: string = 'The most successful IT companies in 2020';
+  public buttonText: string = 'Включить поиск с логикой RxJs, при которой фильтрация будет проходить по ентеру';
+  public search$!: Observable<Event>;
+  public sub$!: any;
 
   constructor(private store: Store) { }
 
   ngOnInit(): void {
+    this.search$ = fromEvent<Event>(
+      document.querySelector('#search') as HTMLInputElement,
+      'change'
+    );
+    this.sub$ = this.search$.pipe(
+        map(event => {
+          return (event.target as HTMLInputElement).value;
+        })).subscribe((value: any) => {
+      console.log(value);
+      this.store.dispatch(search({update: value}));
+    });
   }
 
   search(): void {
-    this.store.dispatch(search({update: this.searchStr}));
+    if (this.toogleSearch) this.store.dispatch(search({update: this.searchStr}));
+  }
+
+  toggle(): void {
+    if (this.toogleSearch) {
+      this.toggleText = 'The most successful IT companies in 2020';
+      this.buttonText = 'Включить поиск с логикой RxJs, при которой фильтрация будет проходить по ентеру';
+    } else {
+      this.toggleText = 'RxJs enabled';
+      this.buttonText = 'Отключить поиск с логикой RxJs';
+    }
   }
 }
