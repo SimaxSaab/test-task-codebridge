29b1c8e4eab1e51d2325f74a506af7ed3a29cad9
Simax Saab <minoreat@gmail.com>
2022-05-05 03:21:04 +0300

    feat: add rxjs popup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss',
+  './popup.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-task-codebridge';
+
+  public sub: Subscription;
+  public popup!: boolean;
+
+  constructor() { 
+    const stream$ = new Observable(observer => {
+      setTimeout(() => {
+        observer.next(false)
+      }, 15000)
+    });
+    this.sub = stream$.subscribe((value: any) => {
+      console.log(value);
+      this.popup = value;
+    });
+    this.popup = true;
+  }
+
+  ngOnInit(): void {
+  }
+
 }
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,13 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable, Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
+
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  // public sub: Subscription;
+  // public popup!: boolean;
+
+  constructor() { 
+    // const stream$ = new Observable(observer => {
+    //   setTimeout(() => {
+    //     observer.next(false)
+    //   }, 10000)
+    // });
+    // this.sub = stream$.subscribe((value: any) => {
+    //   console.log(value);
+    //   this.popup = value;
+    // });
+    // this.popup = true;
+  }
 
   ngOnInit(): void {
   }
